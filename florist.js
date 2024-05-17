a78cd61f1fa3887e14d267b95fd97fa3e81a3f0e
rheavada <84101265+rheavada@users.noreply.github.com>
2024-05-17 15:07:54 -0400

    Create florist.js

---
+++ florist.js
@@ -0,0 +1,4 @@
+function addToCart(productName, type) {
+  alert('Added ' + productName + ' to cart.');
+  // Here you can implement further actions like updating a cart in the backend or frontend
+}
